1486f6b2edb6cccdf2a1dfda15c4db9defc4c09c
ldidone <lucas_crar14@hotmail.com>
2018-09-12 22:13:22 -0300

    Primer publicación | LOGIN + REGISTRACIÓN -  PÁGINA DE INICIO

---
+++ TeLoBusco/Servicios/AccesoDatos/AspNetRolesServicio.cs
@@ -0,0 +1,14 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class AspNetRolesServicio
+    {
+
+    }
+}
---
+++ TeLoBusco/Servicios/AccesoDatos/AspNetUsersServicio.cs
@@ -0,0 +1,15 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utilidades;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class AspNetUsersServicio
+    {
+
+    }
+}
