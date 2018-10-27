fb3ef5bced7adec72e0495d8a2714ceb8089b906
Facu218 <facucai_95@hotmail.com>
2018-10-26 23:28:30 -0300

    Denuncias Funcionando

---
+++ TeLoBusco/RepositorioClases/Denuncia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositorioClases
+{
+    public class Denuncia
+    {
+        public int IdDenuncia { get; set; }
+        public int IdPedido { get; set; }
+        public int IdEstado { get; set; }
+        public string DescripcionPedido { get; set; }
+        public string Motivo { get; set; }
+    }
+}
---
+++ TeLoBusco/TeLoBusco/Models/DenunciasViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace TeLoBusco.Models
+{
+    public class DenunciasViewModel
+    {
+        public int IdDenuncia { get; set; }
+        public int IdPedido { get; set; }
+        public int IdEstado { get; set; }
+
+        [Display(Name = "Descripcion del pedido")]
+        public string DescripcionPedido { get; set; }
+
+        [Required]
+        [Display(Name = "Motivo de la denuncia")]
+        public string Motivo { get; set; }
+    }
+}
