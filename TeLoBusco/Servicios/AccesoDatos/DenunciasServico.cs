448598fb6880e0b2bd017268c9dcd83cb637e53d
ldidone <lucas_crar14@hotmail.com>
2018-10-27 04:08:12 -0300

    Combinacíón
    * Ver pedidos deliverys
    * Finalizar pedidos deliverys
    * Corrección Notificación

---
+++ TeLoBusco/Servicios/AccesoDatos/DenunciasServico.cs
@@ -0,0 +1,40 @@
+﻿using Datos;
+using RepositorioClases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.AccesoDatos
+{
+    public class DenunciasServicio
+    {
+        public static bool Crear(Denuncia denuncia)
+        {
+            using (TeloBuscoEntities db = new TeloBuscoEntities())
+            {
+                try
+                {
+                    Denuncias val = new Denuncias
+                    {
+                        IdDenuncia = denuncia.IdDenuncia,
+                        IdPedido = denuncia.IdPedido,
+                        DescripcionPedido = denuncia.DescripcionPedido,
+                        Motivo = denuncia.Motivo
+                    };
+                    db.Denuncias.Add(val);
+                    db.SaveChanges();
+
+                    return true;
+
+                }
+                catch
+                {
+                    return false;
+                }
+
+            }
+        }
+    }
+}
